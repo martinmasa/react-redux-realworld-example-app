b3dd801f7c35a9737b7cc6dd280350ec73f9678e
Martin <martin@m-ten.net>
2017-06-17 19:15:47 +0100

    Add Login component and sign in navigation to header.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import store from './store';
 
 import App from './components/App';
 import Home from './components/Home';
+import Login from './components/Login';
+
 
 ReactDOM.render((
   <Provider store={ store }>
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
+        <Route path="/login" component={Login} />
       </Route>
     </Router>
   </Provider>
